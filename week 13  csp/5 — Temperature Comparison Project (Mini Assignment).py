9a32839c144cbe17208a558961110e3bfc4ceee7
bsalmeron1 <bsalmeron1426@gmail.com>
2025-12-01 20:53:36 +0000

    weather if else practice

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -5,9 +5,16 @@
 # Write a program that:
 
 # Asks the user for today’s temperature.
+temp=int(input("Whats the temperature today? :"))
 
-# Prints whether it’s cold, warm, or hot using comparison operators.
-
+if temp > 50 and temp <= 85 :
+    print(" the weather is perfect")
+elif temp <= 50 and -10 < temp:
+        print("its very cold")
+elif temp >= 86 and temp <= 109:
+    print("Its very hot")
+else:
+ print("Extreme temp warning")
 # If the temperature is out of range (below -10 or above 110), display “Extreme temperature warning!”
 
 # Starter Code:
